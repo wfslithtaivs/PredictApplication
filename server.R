b9ebefada13bfd303177e157bdd2477a4dddc9c3
wfslithtaivs <w4ts@yahoo.com>
2016-04-19 15:20:09 -0700

    Dictionary added
    Full dictionary added and randomly sampled one to test application (26.8 Mb is too much for reasonable delay).

--- server.R
+++ server.R
@@ -6,12 +6,17 @@ source("algo.R")
 # setting this option. Here we'll raise limit to 15MB.
 options(shiny.maxRequestSize = 15*1024^2)
 
-predict <- function(x){ 
-        
-# BackOff
-        d3 <- subset(fullDict, first == x[1, 2] & second == x[1, 3], select = c(third, freq))
-        d2 <- subset(fullDict, first == NA & second == x[1, 3], select = c(third, freq))
-        return(rbind(d3, d2))
+predict <- function(x){
+
+# Simple BackOff with geometric progression coeffitient - initial level no coeff,
+        # level 1 - 0.4, level 2 - 0.16 etc.
+        res <- data.frame("third" = "the", probs = 1)
+        d3 <- subset(fullDict, first == x[1, 2] & second == x[1, 3], select = c(third, probs))
+        d2 <- subset(fullDict, first == NA & second == x[1, 3], select = c(third, probs))
+        if (length(d2$third) != 0) {res$probs <- res$probs*0.16; d2$probs <- d2$probs*0.4; res <- rbind(d2, res)}
+        if ((length(d3$third) != 0) & (length(d2$third) != 0)) {res <- rbind(d3, res)}
+        if ((length(d3$third) != 0) & (length(d2$third) == 0)) {res$probs <- res$probs*0.16; res <- rbind(d3, res)}
+        return(res)
 }
 
 takeTheLastSome <- function(x){
@@ -20,15 +25,15 @@ takeTheLastSome <- function(x){
 }
 
 shinyServer(function(input, output) {
-        
+
         values <- reactive({
-              takeTheLastSome(input$sentence)      
-        })       
-        
-        output$sentence <- renderPrint({preprocess(input$sentence)}) 
+              takeTheLastSome(input$sentence)
+        })
+
+        output$sentence <- renderPrint({preprocess(input$sentence)})
         output$takeSome <- renderPrint({values()})
-        output$testprediction <- renderTable({data.frame(fullDict)})  
+        output$testprediction <- renderTable({data.frame(head(fullDict))})
         output$realprediction <- renderTable({predict(preprocess(input$sentence))})
-        
+
         }
 )
--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ library(shinythemes)
 
 shinyUI(fluidPage(
                 # theme = "bootstrap.css",
-                
+
                 tags$head(
                         tags$style(HTML("
                         @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
@@ -20,50 +20,50 @@ shinyUI(fluidPage(
                         font-family: 'Lobster';
                         font-weight: 400;
                         line-height: 1.1;
-                        color: #cd5c5c;            
+                        color: #cd5c5c;
                         }
                         body {
                         background-color: #ffebcd;
-                        }                
-                        text/css", ".span8 .well { background-color: #ff4040; } 
+                        }
+                        text/css", ".span8 .well { background-color: #ff4040; }
                         "))
                 ),
-                
+
                   headerPanel("Next Word Prediction"),
-                  
+
                   sidebarPanel(
                           withTags({
-                                  div(class="header", checked=NA, style = "color:grey", 
+                                  div(class="header", checked=NA, style = "color:grey",
                                       p("Enter the word or sentence, don't forget about symbols and numbers, press the Submit button and enjoy the artificial intelligence")
                                   )
-                          }), 
-                          
+                          }),
+
                           textInput("sentence", "Input here:", value = "Keep typing ..."),
-                 
+
                           submitButton('Submit'),
-                
+
                           withTags({
-                                        div(class="header", checked=NA, style = "color:grey", 
-                                        p(""), 
+                                        div(class="header", checked=NA, style = "color:grey",
+                                        p(""),
                                         p("Please, peer me softly and ---->"),
-                                        a(href="https://github.com/wfslithtaivs/", "Review code here")
+                                        a(href="https://github.com/wfslithtaivs/PredictApplication", "Review code here")
                                 )
-                        }) 
+                        })
                   ),
-                
-                  
+
+
                   mainPanel(
                           h4('You entered:'),
-                          verbatimTextOutput("sentence"), 
+                          verbatimTextOutput("sentence"),
                           h4('Sentence stat:'),
                           verbatimTextOutput("takeSome"),
                           h4('Top 5 predicted:'),
                           tableOutput("testprediction"),
                           h4('Top 5 predicted:'),
                           tableOutput("realprediction")
-                          
-                         
+
+
                   )
-                    
-        )             
+
+        )
 )
